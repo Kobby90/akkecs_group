ee4b5f469644a624421326f0d7bcb77bbec0954d
Kobby90 <daniel.cudjoe1990@gmail.com>
2025-06-03 23:30:28 +0000

    Configure for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',  // This will generate static files
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  trailingSlash: true, // This helps with static hosting
 };
- 
+
 module.exports = nextConfig; 
