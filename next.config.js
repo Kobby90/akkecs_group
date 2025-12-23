651473440c8ace080b1ec16f152dd7be4cf5f4f7
Kobby90 <daniel.cudjoe1990@gmail.com>
2025-12-23 19:20:07 +0000

    Build Error Fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   //   unoptimized: true, // Removed to allow Next.js image optimization
   // },
   trailingSlash: true, // Preserved
+  outputFileTracing: false, // Fix for "RangeError: Maximum call stack size exceeded" during build tracing
 };
 
 module.exports = nextConfig; 
